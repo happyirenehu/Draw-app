4ca3e4b660f7391d01929ba6a4a02f615582401e
happyirenehu <happyirenehu@gmail.com>
2021-08-05 19:29:44 +0800

    Update editableShapesTool.js

--- draw-app-masterV2/editableShapesTool.js
+++ draw-app-masterV2/editableShapesTool.js
@@ -1,17 +1,11 @@
 function editableShapesTool(){
 	
-	this.name = "sprayCanTool";
-	this.icon = "assets/sprayCan.jpg";
+	this.name = "editableShapesTool";
+	this.icon = "assets/editable.jpg";
 
-	var points = 13;
-	var spread = 10;
+
 
 	this.draw = function(){
-		var r = random(5,10);
-		if(mouseIsPressed){
-			for(var i = 0; i < points; i++){
-				point(random(mouseX-spread, mouseX + spread), random(mouseY-spread, mouseY+spread));
-			}
-		}
+		
 	};
 }
